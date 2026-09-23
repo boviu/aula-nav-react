36c5535bb93810cb315b8d57cdfb32ca7fe7a0f3
boviu <leticiaanti2023@gmail.com>
2026-09-23 14:48:55 +0000

    commit3

--- projeto-react/src/App.jsx
+++ projeto-react/src/App.jsx
@@ -3,31 +3,24 @@ import './App.css'
 import Home from './pages/Home'
 import Contato from './pages/Contato'
 import Sobre from './pages/Sobre'
-
-function App() {
-  const [pagina, setPagina] = useState('home')
-
-  const renderizarPagina=()=>{
-    if (pagina === 'home') return <Home/>
-    if (pagina === 'sobre') return <Sobre/>
-    if (pagina === 'contato') return <Contato/>
-  }
-
+// router
+import {Link, Routes, Route} from 'react-router'
 
   return (
     <div>
       <h1>MEU SITE FAVORITO EM REACT (O PRIMEIRO)</h1>
-      <nav>
-        <button onClick={()=>setPagina('home')}>Home</button>
-        <button onClick={()=>setPagina('sobre')}>Sobre</button>
-        <button onClick={()=>setPagina('contato')}>Contato</button>
-      </nav>
-
-      <div>
-        {renderizarPagina()}
-      </div>
+      
+      <Link to='/'>Home</Link>
+      <Link to='/sobre'>Sobre</Link>
+      <Link to='/contato'>Contato</Link>
+      <hr/>
+      <Routes>
+        <Route path = '/' element = {<Home/>}/>
+        <Route path='/sobre' element={<Sobre/>}/>
+        <Route path='/contato' element={<Contato/>}/>
+      </Routes>
     </div>
   )
-}
+
 
 export default App
